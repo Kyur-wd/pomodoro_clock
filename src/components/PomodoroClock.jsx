a5b4dd19e7cd0176f4cfaf5b4d3068cbb8efd93f
Bilal Ahmad <kyuranx@outlook.de>
2019-06-30 18:31:21 +0200

    Added more basic functionality and some refactoring

--- src/components/PomodoroClock.jsx
+++ src/components/PomodoroClock.jsx
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
+import { existsTypeAnnotation } from '@babel/types';
 
 class PomodoroClock extends Component {
     constructor() {
         super();
         this.resetClock = this.resetClock.bind(this);
-        this.increaseBreakLength = this.increaseBreakLength.bind(this);
-        this.decreaseBreakLength = this.decreaseBreakLength.bind(this);
+        this.updateTimerSetting = this.updateTimerSetting.bind(this);
+        this.startOrStopTimer = this.startOrStopTimer.bind(this);
     }
 
     state = {
         breakLength: 5,
         sessionLength: 25,
-        timeLeft: 25
+        timeLeft: 25,
+        timerIsRunning: false,
+        seconds: 60
+    }
+
+    startOrStopTimer() {
+        if (this.state.timerIsRunning) clearInterval();
+        else setInterval({}, 1000);
+
+        this.setState({ timerIsRunning: !this.state.timerIsRunning });
     }
 
     resetClock() {
         this.setState({ breakLength: 5, sessionLength: 25, timeLeft: 25 });
     }
 
-    increaseBreakLength() {
-        this.setState(prevState => ({
-            breakLength: prevState.breakLength + 1
-        }));
-    }
+    updateTimerSetting(timerName, timeAmount) {
+        if (this.state.timerIsRunning) return;
 
-    decreaseBreakLength() {
-        if (this.state.breakLength > 1) {
+        if (this.state[timerName] + timeAmount >= 1 && this.state[timerName] + timeAmount <= 60) {
             this.setState(prevState => ({
-                breakLength: prevState.breakLength - 1
+                [timerName]: prevState[timerName] + timeAmount
             }));
         }
     }
@@ -37,15 +43,15 @@ class PomodoroClock extends Component {
             <main>
                 <p id="break-label">Break Length</p>
                 <p id="break-length">{this.state.breakLength}</p>
-                <button id="break-increment" onClick={this.increaseBreakLength}>+</button>
-                <button id="break-decrement" onClick={this.decreaseBreakLength}>-</button>
+                <button id="break-increment" onClick={() => { this.updateTimerSetting("breakLength", 1) }}>+</button>
+                <button id="break-decrement" onClick={() => { this.updateTimerSetting("breakLength", -1) }}>-</button>
                 <p id="session-label">Session Length</p>
                 <p id="session-length">{this.state.sessionLength}</p>
-                <button id="session-decrement">+</button>
-                <button id="session-increment">-</button>
+                <button id="session-increment" onClick={() => { this.updateTimerSetting("sessionLength", 1) }}>+</button>
+                <button id="session-decrement" onClick={() => { this.updateTimerSetting("sessionLength", -1) }}>-</button>
                 <p id="timer-label">Session</p>
-                <p id="time-left">{this.state.timeLeft}</p>
-                <button id="start_stop">Start/Stop</button>
+                <p id="time-left">Übrig: {this.state.timeLeft}</p>
+                <button id="start_stop" onClick={this.startOrStopTimer}>Start/Stop</button>
                 <button id="reset" onClick={this.resetClock}>Reset</button>
             </main>
         );
